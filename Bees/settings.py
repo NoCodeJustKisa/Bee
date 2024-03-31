b099167cb77593d4edd0c60a850023288b8c3840
Kisa <saynosert@gmail.com>
2024-03-31 19:38:58 +0300

    security improvements + templates

--- Bees/settings.py
+++ Bees/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gg8qrfhf4y1zmnrd1*4gst=l9zcs^(j36!t58y*q5j8^a_tmbk'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +83,11 @@ WSGI_APPLICATION = 'Bees.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bees_db',
-        'USER': 'your_database_user',  # ИЗМЕНИТЬ НА СВОЕГО ПОЛЬЗОВАТЕЛЯ
-        'PASSWORD': 'your_database_password',  # ИЗМЕНИТЬ НА СВОЙ ПАРОЛЬ
-        'HOST': 'localhost',  # or your PostgreSQL server's IP address
-        'PORT': '5432',  # default PostgreSQL port
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
 
     }
 }
