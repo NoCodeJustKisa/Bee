4214dab719b9b572d84e74a62ea172901dd9107c
Kisa <saynosert@gmail.com>
2024-04-19 22:29:12 +0300

    Daily check-in is now possible

--- beehive/views.py
+++ beehive/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
+import datetime, zoneinfo
+from zoneinfo import ZoneInfo
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm, CheckinForm
+from beehive.models import User as BeehiveUser
+from beehive.models import Record
 # Create your views here.
 
 
@@ -22,17 +26,32 @@ def register(request):
 
 @login_required(redirect_field_name='login')
 def mainpage(request):
-    if request.method == "POST":
-        form = CheckinForm(request.POST)
+    current_user = request.user #берем джанговского юзера
+    beehive_user = BeehiveUser.objects.get(username=current_user) #а теперь берем юзера расширенного с тзшкой
+    user_timezone = ZoneInfo(beehive_user.timezone) #пихаем строку в ZoneInfo и получаем тз объект с тз пользователя
+    current_date = datetime.datetime.now(user_timezone).date() #Получаем че там за день у пользователя сейчас
+    records = Record.objects.filter(user=current_user) #берем все записи пользователя
+    record = None #тут типа запись которая может и есть а может и нет а может...
+    for rec in records: #проходимся по записям (ето ужас канешна полный лицо сервера имаджинируйте :D)
+        created_at_user_tz = rec.created_at.astimezone(user_timezone) #переводим время записи в тз пользователя
+        if created_at_user_tz.date() == current_date:  #если дата записи равна текущей дате (в тз юзера)
+            record = rec #то запись есть и мы ее чпок и выходим из цикла
+            break
+
+    if request.method == "POST": # так тут вроде все спокойно никаких всяких ну ты поняла
+        form = CheckinForm(request.POST, instance=record) #еси запись имеется то передаем ее еси нет то делаем новую
         if form.is_valid():
             record = form.save(commit=False)
             record.user = request.user
             record.save()
             return redirect('main')
-        else:
-            return render(request, "checkin.html", context={'form': form})
     else:
-        form = CheckinForm
+        if record and record.mood and record.activity:
+            # If a record exists for today and both fields are filled, show a message
+            return render(request, "checkin.html", context={'message': 'Вы уже отмечались сегодня', 'title': 'Beehive | Отметиться'})
+        else:
+            # If no record exists or if a record exists but a field is blank, show the form
+            form = CheckinForm(instance=record)
     return render(request, "checkin.html", context={'title': 'Beehive | Отметиться', 'form': form})
 
 
