01b0c4d4d60bf0bd21d0cdb45a484bf5b1a55dd0
Kisa <saynosert@gmail.com>
2024-05-08 20:30:48 +0300

    Analytics finished

--- beehive/tests.py
+++ beehive/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+import pytest
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
+from beehive.models import Record, Note, Message
+from beehive.models import User as BeehiveUser
+from zoneinfo import ZoneInfo
+from datetime import datetime
 
+class BeehiveViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.test_user = User.objects.create_user(username='testuser', password='12345')
+        self.test_beehive_user = BeehiveUser.objects.create(username='testuser', timezone='UTC')
+        self.test_note = Note.objects.create(user=self.test_user, text='Test note')
+        self.test_record = Record.objects.create(user=self.test_user, mood=5, activity=5)
+        self.test_message = Message.objects.create(user=self.test_user, message='Test message', response='Test response')
+
+    def test_register_view(self):
+        response = self.client.post('/register/', {'username': 'testuser2', 'password1': '12345', 'password2': '12345'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_mainpage_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_creation_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post('/createnote/', {'text': 'Test note 2'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_history_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/history/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_chat_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post('/chat/', {'message': 'Test message 2'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_analytics_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post('/analytics/', {'start_date': '2022-01-01', 'end_date': '2022-12-31'})
+        self.assertEqual(response.status_code, 200)
 # Create your tests here.
